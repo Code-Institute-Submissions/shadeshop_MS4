8fe23923f4cb9638bb88a695120fe0828810dfe4
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-24 20:08:26 +0000

    Added limited search functionality

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.women_products, name='women_products'),
+    path('', views.all_products, name='products'),
     path('<product_id>', views.product_detail, name='product_detail')
 ]
