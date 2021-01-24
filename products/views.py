f590ba3b070732018a3035f90e902415093e3e62
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-24 21:21:20 +0000

    Added producting filtering by gender, brand and sale items

--- products/views.py
+++ products/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Brand
 
 
 # Create your views here.
@@ -12,9 +12,32 @@ def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    brands = Brand.objects.all()
     query = None
+    gender = None
+    brand = None
+    sale = None
 
     if request.GET:
+        if 'sale' in request.GET:
+            sale = request.GET['sale']
+            if 'gender' in request.GET:
+                gender = request.GET['gender']
+                queries = Q(gender__in=gender) & Q(sale=sale)
+                products = products.filter(queries)
+            else:
+                products = products.filter(sale=sale)
+        else:
+            if 'gender' in request.GET:
+                gender = request.GET['gender']
+                if 'brand' in request.GET:
+                    brand = request.GET['brand']
+                    queries = Q(gender__in=gender) & Q(brand__in=brand)
+                    products = products.filter(queries)
+                    brand = brands.filter(id=brand)
+                else:
+                    products = products.filter(gender__in=gender)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -28,7 +51,12 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'brand': brand,
+        'gender': gender,
+        'sale': sale,
     }
+
+    print(context)
 
     return render(request, 'products/products.html', context)
 
