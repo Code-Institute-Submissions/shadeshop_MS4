3b1c50bf39ae13e8c17bdeac0dccccc904720bf4
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-27 20:17:45 +0000

    Added checkout views and templates

--- checkout/models.py
+++ checkout/models.py
@@ -66,7 +66,10 @@ class OrderLineItem(models.Model):
         Override the original save method to set the lineitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        if self.product.sale is True:
+            self.lineitem_total = self.product.saleprice * self.quantity
+        else:
+            self.lineitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
