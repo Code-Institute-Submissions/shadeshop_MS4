94d0bde6b177e5dfdeef08cd3e55b1274989ba07
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-31 19:07:21 +0000

    Removed Secret Key and set Debug

--- shadeshop_MS4/settings.py
+++ shadeshop_MS4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '164x-@tmn(dwg-)fjkx#srnrhsid7(q%ej)*4uk)4o&cqvky_l'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['shadeshop-ms4.herokuapp.com', 'localhost']
 
