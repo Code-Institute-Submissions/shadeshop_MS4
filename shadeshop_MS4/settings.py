fd2ff177a867522fadfaff194ef945bcd3c18730
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-31 18:40:32 +0000

    Created new heroku database as previous uri was commited

--- shadeshop_MS4/settings.py
+++ shadeshop_MS4/settings.py
@@ -122,20 +122,14 @@ WSGI_APPLICATION = 'shadeshop_MS4.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+ DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
-DATABASES = {
-    'default': dj_database_url.parse(
-        'postgres://ykfvtuhozxweqc:'
-        'bd0081b90e7b51bf393377ae5976c0b0212ad592dfa1e0e638ee3a7b3c268aed'
-        '@ec2-46-137-156-205.eu-west-1.compute.amazonaws.com:'
-        '5432/d6923qt41e49o')
-}
+
 
 
 # Password validation
