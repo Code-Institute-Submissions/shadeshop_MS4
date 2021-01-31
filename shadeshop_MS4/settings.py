eef01e8a72f469ff8b8ced6d96b86f13d1faad29
allanahmurphy <allanahtmurphy@gmail.com>
2021-01-31 19:45:02 +0000

    Setup AWS S3 static files storage settings for heroku

--- shadeshop_MS4/settings.py
+++ shadeshop_MS4/settings.py
@@ -48,9 +48,11 @@ INSTALLED_APPS = [
     'checkout',
     'profiles',
     'wishlist',
+    
 
     # Other
-    'crispy_forms'
+    'crispy_forms',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -175,6 +177,26 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+if 'USE_AWS' in os.environ:
+    # Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'shadeshop-ms4'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+
+
+# Delivery Details
 FREE_DELIVERY_THRESHOLD = 70
 STANDARD_DELIVERY_PERCENTAGE = 5
 
@@ -186,9 +208,3 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 # Contact Email
 DEFAULT_FROM_EMAIL = 'shadeshop@example.com'
-
-# Setup Connection Variables for AWS server for image storage
-BUCKET_NAME = "shadeshop"
-AWS_KEY_ID = os.environ.get("AWS_KEY_ID")
-AWS_SECRET_KEY = os.environ.get("AWS_SECRET_KEY")
-S3_LOCATION = 'http://{}.s3.amazonaws.com/'.format(BUCKET_NAME)
